94f008fa44ed26f96ac7f15547073f201fcb3bb6
Josh Tai <jitai@ucsc.edu>
2020-08-16 21:06:33 -0700

    functional loginmodal

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,10 +1,10 @@
 import React, { Fragment } from 'react';
-import {Button, Modal } from 'react-bootstrap';
+import {Button, Modal, Form } from 'react-bootstrap';
 
 export interface LoginModalProps {
     show: boolean;
     onCancel: () => void;
-    onLogin: () => void;
+    onLogin: (arg0: string, arg1: string) => void;
 }
 
 
@@ -12,18 +12,47 @@ export default (props: LoginModalProps) => (
 
     <Fragment>
       <Modal show={props.show}>
+      <Form onSubmit={(e) => {e.preventDefault(); 
+
+                              const emailForm = document.querySelector("#email") as HTMLInputElement;
+                              const passwordForm = document.querySelector("#password") as HTMLInputElement;
+
+                              const login = (emailForm && passwordForm) ? props.onLogin( emailForm.value, passwordForm.value) : null;
+                            
+                            
+                            }
+                    }>
         <Modal.Header closeButton>
           <Modal.Title>Login</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Hello world!</Modal.Body>
+        <Modal.Body>
+
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control id="email" type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control id="password" type="password" placeholder="Password" />
+  </Form.Group>
+
+  </Modal.Body>
+
+
+
         <Modal.Footer>
           <Button variant="secondary" onClick={() => props.onCancel()}>
             Cancel
           </Button>
-          <Button variant="primary" onClick={() => props.onLogin()}>
+          <Button variant="primary" type="submit">
             Login
           </Button>
         </Modal.Footer>
+        </Form>
       </Modal>
     </Fragment>
 )
